7fca9e556e31f8d462cd083e76b0589a9182277e
William Dobbins <will.dobbins@gmail.com>
2019-10-05 16:10:12 -0700

    Iniital flask setup

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'ReadMe (BE)'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
